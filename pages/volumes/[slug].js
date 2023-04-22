abb1510adfb3523259c60e79f46d0e9332ab9a69
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-22 22:55:03 +0200

    add slug.js

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -1,30 +1,33 @@
 export default function VolumeDetail() {
-    const volume = volumes.find(
-        ({ slug }) => slug === "the-fellowship-of-the-ring"
-      );
-    
-    return (
-        <>
-        <Link href="../volumes">All Volumes</Link>
-        <h1>Fellowship of the Ring</h1>
-        <p>{volume.description}</p>
-        <ul>
-            <li>{volume.title} {volumes[0].books[0].ordinal}</li>
-            <li>{volume.books[1].title} {volume.books[1].ordinal}</li>
-        </ul>
-        <Image
-             src={volume.cover}
-             alt={volume.title}
-             width={140}
-             height={230}
-        >
-        </Image>
-        <div>
+  const volume = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+
+  return (
+    <>
+      <Link href="../volumes">All Volumes</Link>
+      <h1>Fellowship of the Ring</h1>
+      <p>{volume.description}</p>
+      <ul>
+        <li>
+          {volume.title} {volumes[0].books[0].ordinal}
+        </li>
+        <li>
+          {volume.books[1].title} {volume.books[1].ordinal}
+        </li>
+      </ul>
+      <Image
+        src={volume.cover}
+        alt={volume.title}
+        width={140}
+        height={230}
+      ></Image>
+      <div>
         <Link href="/volumes">Previous</Link>
-        </div>
-        <div>
+      </div>
+      <div>
         <Link href="/volumes/the-two-towers">Next</Link>
-        </div>
-        </>
-        )
+      </div>
+    </>
+  );
 }
