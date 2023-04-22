700c7b09661c28552995491a34553c1d17fcd332
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-23 00:43:08 +0200

    finish dinamic routes and random button

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -1,12 +1,36 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { volumes } from "../../public/resources 3/lib/data";
+import Image from "next/image";
+
 export default function VolumeDetail() {
-  const volume = volumes.find(
-    ({ slug }) => slug === "the-fellowship-of-the-ring"
-  );
+  const router = useRouter();
+  // /volumes/[slug]
+  const { slug } = router.query;
+
+  const volume = volumes.find((volume) => volume.slug === slug);
+
+  const i = volumes.findIndex((volume) => volume.slug === slug);
+  const length = volumes.length;
+  const prevSlug = volumes[i === 0 ? 0 : i - 1].slug;
+  // i = 2  length = 3 : nextSlug = 3   length[3] => undefined
+  const nextSlug = volumes[i >= length - 1 ? length - 1 : i + 1].slug;
+
+  function getRandomElement(array) {
+    console.log("random!");
+
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleRandomButtonClick() {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
 
   return (
     <>
       <Link href="../volumes">All Volumes</Link>
-      <h1>Fellowship of the Ring</h1>
+      <h1>{volume.title}</h1>
       <p>{volume.description}</p>
       <ul>
         <li>
@@ -23,10 +47,13 @@ export default function VolumeDetail() {
         height={230}
       ></Image>
       <div>
-        <Link href="/volumes">Previous</Link>
+        <button onClick={handleRandomButtonClick}>Go to random volume!</button>
       </div>
       <div>
-        <Link href="/volumes/the-two-towers">Next</Link>
+        <Link href={`/volumes/${prevSlug}`}>Previous</Link>
+      </div>
+      <div>
+        <Link href={`/volumes/${nextSlug}`}>Next</Link>
       </div>
     </>
   );
